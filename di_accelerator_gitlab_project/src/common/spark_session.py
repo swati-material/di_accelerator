35f555e4ec8025d8844602bc424db000251d407a
akash.kumar <kumara@crohnscolitisfoundation.org>
2026-04-20 10:25:56 +0530

    commiting the changes after fixing the bug

--- di_accelerator_gitlab_project/src/common/spark_session.py
+++ di_accelerator_gitlab_project/src/common/spark_session.py
@@ -1,11 +1,11 @@
 import os
 from pyspark.sql import SparkSession
 
-def create_spark_session(app_name: str = "DI_Accelerator_POC") -> SparkSession:
+def create_spark_session(app_name: str = "LOCAL_TEST") -> SparkSession:
     return (
         SparkSession.builder
+        .master("local[*]")
         .appName(app_name)
-        .config("spark.sql.session.timeZone", "UTC")
         .getOrCreate()
     )
 
